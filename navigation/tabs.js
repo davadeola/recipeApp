4efa5e5e86d372012cfc22ecedd68165a499512a
davadeola <adeoladavid15@gmail.com>
2022-05-05 12:46:53 +0300

    set up the default setting of the bottom tab bar

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -10,7 +10,22 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+          elevation: 0,
+          backgroundColor: COLORS.white,
+          borderTopColor: 'transparent',
+          height: 100,
+        },
+      }}
+      tabBarOptions={{}}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Search" component={Home} />
       <Tab.Screen name="Bookmark" component={Home} />
