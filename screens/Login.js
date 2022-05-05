e382340af19ed218449e95c87de77175bed67125
davadeola <adeoladavid15@gmail.com>
2022-05-05 10:37:35 +0300

    added a background image on the login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    TouchableOpacity
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  ImageBackground,
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-const Login = ({ navigation }) => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Text>Login</Text>
-            <TouchableOpacity
-                onPress={() => navigation.replace("Home")}
-            >
-                <Text>Navigate to Home</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+import {COLORS, images, SIZES, FONTS} from '../constants';
+
+const Login = ({navigation}) => {
+  const renderHeader = () => (
+    <View style={{height: SIZES.height > 700 ? '65%' : '60%'}}>
+      <ImageBackground
+        source={images.loginBackground}
+        style={{flex: 1, justifyContent: 'flex-end'}}
+        resizeMode="cover"></ImageBackground>
+    </View>
+  );
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.black,
+      }}>
+      <StatusBar barStyle="light-content" />
+      {/* Header */}
+
+      {renderHeader()}
+    </View>
+  );
+};
 
 export default Login;
