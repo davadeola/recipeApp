0f3cbd1fe56c5352fbc62e2f0f7f5e572bc2822e
davadeola <adeoladavid15@gmail.com>
2022-05-09 10:57:50 +0300

    added the header and the search bar in Home

--- screens/Home.js
+++ screens/Home.js
@@ -13,6 +13,58 @@ import {FONTS, COLORS, SIZES, icons, images, dummyData} from '../constants';
 import {CategoryCard} from '../components';
 
 const Home = ({navigation}) => {
+  const renderHeader = () => (
+    <View
+      style={{
+        flexDirection: 'row',
+        marginHorizontal: SIZES.padding,
+        alignItems: 'center',
+        height: 80,
+      }}>
+      <View style={{flex: 1}}>
+        <Text style={{color: COLORS.darkGreen, ...FONTS.h2}}>
+          Hello David Adeola
+        </Text>
+        <Text style={{marginTop: 3, color: COLORS.gray, ...FONTS.body3}}>
+          What do you want to cook today?
+        </Text>
+      </View>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Image
+          source={images.UserProfile1}
+          style={{width: 40, height: 40, borderRadius: 20}}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderSearchBar = () => (
+    <View
+      style={{
+        flexDirection: 'row',
+        height: 50,
+        alignItems: 'center',
+        marginHorizontal: SIZES.padding,
+        paddingHorizontal: SIZES.radius,
+        backgroundColor: COLORS.lightGray,
+        borderRadius: 10,
+      }}>
+      <Image
+        source={icons.search}
+        style={{width: 20, height: 20, tintColor: COLORS.gray}}
+      />
+      <TextInput
+        style={{
+          marginLeft: SIZES.radius,
+          ...FONTS.body3,
+        }}
+        placeholderTextColor={COLORS.gray}
+        placeholder="Search Recipes"
+      />
+    </View>
+  );
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
       <FlatList
@@ -29,6 +81,12 @@ const Home = ({navigation}) => {
             onPress={() => navigation.navigate('Recipe', {recipe: item})}
           />
         )}
+        ListHeaderComponent={
+          <View>
+            {renderHeader()}
+            {renderSearchBar()}
+          </View>
+        }
         ListFooterComponent={<View style={{marginBottom: 100}} />}
       />
     </SafeAreaView>
