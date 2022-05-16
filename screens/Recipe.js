d91fc5461849fffd803c1ff66f1a947e338c112d
davadeola <adeoladavid15@gmail.com>
2022-05-16 16:50:41 +0300

    retrieved the recipe item from Home screen

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import {
-    View,
-    Text
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Animated,
+  Platform,
 } from 'react-native';
+import {set} from 'react-native-reanimated';
 
-const Recipe = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Text>Recipe</Text>
-        </View>
-    )
-}
+import {SIZES, FONTS, COLORS, icons} from '../constants';
+
+const HEADER_HEIGHT = 350;
+
+const Recipe = ({navigation, route}) => {
+  const [selectedRecipe, setSelectedRecipe] = React.useState(null);
+
+  React.useEffect(() => {
+    let {recipe} = route.params;
+    setSelectedRecipe(recipe);
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Recipe</Text>
+    </View>
+  );
+};
 
 export default Recipe;
